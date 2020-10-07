2e4a6510fd08008ee7e3d57ce3cb257cbe91d03e
Maxime Autechaud <54284476+MaximeAutechaud@users.noreply.github.com>
2020-10-07 21:42:24 +0200

    Update form.php

--- form.php
+++ form.php
@@ -16,23 +16,23 @@ $select = [
 <form action="thanks.php" method="post">
     <div>
         <label for="lastname">Nom :</label>
-        <input type="text" id="lastname" name="user_lastname">
+        <input required type="text" id="lastname" name="user_lastname">
     </div>
     <div>
         <label for="firstname">Prénom :</label>
-        <input type="text" id="firstname" name="user_firstname">
+        <input required type="text" id="firstname" name="user_firstname">
     </div>
     <div>
         <label for="phone">Téléphone :</label>
-        <input type="tel" id="phone" name="user_phone">
+        <input required type="tel" id="phone" name="user_phone">
     </div>
     <div>
         <label for="mail">e-mail :</label>
-        <input type="email" id="mail" name="user_mail" placeholder="exemple@mail.com">
+        <input required type="email" id="mail" name="user_mail" placeholder="exemple@mail.com">
     </div>
     <div>
         <label for="subject">Subject</label>
-        <select name="subject" class="form-control" id="subject">
+        <select required name="subject" class="form-control" id="subject">
             <?php foreach ($select as $subject) { ?>
                 <option value=" <?= $subject ?>"><?= $subject ?></option>
             <?php } ?>
@@ -40,7 +40,7 @@ $select = [
     </div>
     <div>
         <label for="msg">Message :</label>
-        <textarea id="msg" name="user_message"></textarea>
+        <textarea required id="msg" name="user_message"></textarea>
     </div>
     <div>
         <button type="submit" name="btn"> Envoyez !</button>
